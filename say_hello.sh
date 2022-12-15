5bf0f50dceb0b9ba1dc63d6c68d120d2ec3b6606
drnarenv <120614218+drnarenv@users.noreply.github.com>
2022-12-15 02:14:51 -0800

    Delete say_hello.sh

--- say_hello.sh
+++
@@ -1,4 +0,0 @@
-go get github.com/fullstorydev/grpcurl
-go install github.com/fullstorydev/grpcurl/cmd/grpcurl
-
-grpcurl -v -plaintext -d '{"name":"bob"}' localhost:50051 protos.HelloWorld/SayHello
